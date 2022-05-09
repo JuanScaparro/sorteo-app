fff59449e89eb0019bafc21c087096be1ab1b346
JuanScaparro <juanscaparro@gmail.com>
2022-05-09 17:59:36 -0300

    feat: initial config

---
+++ src/interfaces/iPlayer.interface.ts
@@ -0,0 +1,18 @@
+export interface IPlayer {
+  id: string,
+  firstName: string,
+  lastName: string,
+  maidenName: string,
+  age: number,
+  gender: string,
+  birthDate: string,
+  address: {
+    address: string,
+    city: string,
+    coordinates: {
+    lat: number,
+    lng: number
+    },
+  },
+  lotteryNumber: number
+};
--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,10 @@
+import { v4 as uuidv4 } from 'uuid';
+import swal from 'sweetalert';
+import { IPlayer } from './interfaces/iPlayer.interface';
+
 import './style.css'
+
+
 
 const app: HTMLDivElement = <HTMLDivElement>document.querySelector<HTMLDivElement>('#app');
 
@@ -6,3 +12,56 @@ app.innerHTML = `
   <h1>Sorteo App!</h1>
   <p>Tu app de sorteo favorita</p>
   <h2>Proximamente...</h2>`;
+
+
+const lotteryNumRef: number = 1000000;
+
+let players: IPlayer[] = [];
+
+
+
+
+
+async function loadPayers() {
+  const url = 'https://dummyjson.com/users?limit=100';
+
+  try {
+    const response = await fetch( url );
+    const result = await response.json();
+    generateId( result.users )
+  } catch( error ) {
+    errorMessage( error )
+  };
+};
+
+function errorMessage( _error: any ) {
+  swal({
+    title: "Ooohh nooo!",
+    text: "Parece que hay un error, volvé a intentarlo nuevamente...",
+    icon: "error",
+  });;
+};
+
+function generateId( players: IPlayer[] ) {
+  players.forEach(player => player.id = uuidv4() );
+  savePlayers( players );
+};
+
+function savePlayers(data : IPlayer[]) {
+  players = data;
+  console.log('savePlayers =>', players)
+  generateLotteryNumber()
+  
+};
+
+function generateLotteryNumber() {
+  let lotteryNumber: number = Math.round( Math.random() * lotteryNumRef )
+  console.log(lotteryNumber)
+};
+
+
+function init() {
+  loadPayers();
+};
+
+init();
